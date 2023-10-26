1b26e2edbce6e6357d78cb85f7bfb72acd735a59
Erik <santicasas667@gmail.com>
2023-10-26 15:21:42 -0500

    README delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const clientRouter = require('./clients.js');
+// TODO: Completa tu código aquí ⬇️
 
-const app = express();
-const PORT = 3000;
 
-// Middleware para manejar rutas de clientes
-app.use('/clients', clientRouter);
 
-app.get("/", (req, res) => {
-  res.send("Bienvenid@ al laboratorio de routing");
-});
 
-// Se recomienda no editar ni eliminar la instancia del servidor.
+app.get("/", (req,res)=>{
+  res.send("Bienvenid@ al laboraotio de routing")
+})
+//Se recomienda no editar ni eliminar la instancia del servidor.
 // Instancia del servidor
 const server = app.listen(PORT, () => {
   console.log(`listening on port http://localhost:${PORT}`);
