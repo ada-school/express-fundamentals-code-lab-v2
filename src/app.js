dcd1033cf3a9cdac6ae284833a82ee5232eca726
Erik <santicasas667@gmail.com>
2023-10-26 08:26:13 -0500

    lab-intro-express

--- src/app.js
+++ src/app.js
@@ -1,11 +1,5 @@
 // TODO: Completa tu código aquí ⬇️
 
-
-
-
-app.get("/", (req,res)=>{
-  res.send("Bienvenid@ al laboraotio de routing")
-})
 //Se recomienda no editar ni eliminar la instancia del servidor.
 // Instancia del servidor
 const server = app.listen(PORT, () => {
