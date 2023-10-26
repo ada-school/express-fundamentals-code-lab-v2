4947aecf3107e884d67009f8efd600557214d693
Erik <santicasas667@gmail.com>
2023-10-26 15:20:37 -0500

    prueba

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
-// TODO: Completa tu código aquí ⬇️
+const express = require('express');
+const clientRouter = require('./clients.js');
 
+const app = express();
+const PORT = 3000;
 
+// Middleware para manejar rutas de clientes
+app.use('/clients', clientRouter);
 
+app.get("/", (req, res) => {
+  res.send("Bienvenid@ al laboratorio de routing");
+});
 
-app.get("/", (req,res)=>{
-  res.send("Bienvenid@ al laboraotio de routing")
-})
-//Se recomienda no editar ni eliminar la instancia del servidor.
+// Se recomienda no editar ni eliminar la instancia del servidor.
 // Instancia del servidor
 const server = app.listen(PORT, () => {
   console.log(`listening on port http://localhost:${PORT}`);
--- src/clients.js
+++ src/clients.js
@@ -5,4 +5,16 @@ router.get("/", function (req, res) {
   res.send("Ruta clientes");
 });
 
+router.get("/list/:limit", (req, res) => {
+  const limit = req.params.limit;
+  res.send(`Listado de ${limit} autos`);
+});
+
+router.get("/filter", (req, res) => {
+  const query = req.query;
+  const filterType = query.filter;
+
+  res.send(`Resultados filtrados por ${filterType}`);
+});
+
 module.exports = router;
